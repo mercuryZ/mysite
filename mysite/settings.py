8849a2b9a69211c37c8dc277bbbc81f9af5b2c15
mercuryZ <15210820708@163.com>
2020-10-11 23:32:42 +0800

    INSTALLED_APPS && DATABASES

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vote',
+        'HOST': os.environ.get('MYSITE_HOST'),
+        'USER': os.environ.get('MYSITE_USER'),
+        'PASSWORD': os.environ.get('MYSITE_PASSWD'),
+        'CHARSET': 'utf8',
+        'TIME_ZONE': 'Asia/Chongqing',
     }
 }
 
