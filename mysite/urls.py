426954de6e4db15f7bbda67497e1002cace41f74
mercuryZ <15210820708@163.com>
2020-10-26 09:31:13 +0800

    book_detail

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url = '/polls/')),
+    path('', RedirectView.as_view(url = '/catalog/')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
